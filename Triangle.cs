35c58e0f35e779c9e5afd3952a1ebd5d5bfba5c6
Alex Domin <alexsatchdomin@gmail.com>
2022-12-12 20:17:18 -0500

    committing old project to online repo

---
+++ Triangle.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Media;
+
+namespace Space_Invaders
+{
+    internal class Triangle
+    {
+        public string Tag { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public SolidColorBrush Fill { get; set; }
+        public SolidColorBrush Stroke { get; set; }
+    }
+}
